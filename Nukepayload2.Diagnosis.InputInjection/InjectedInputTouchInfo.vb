0ebd8798cac04cb38c67aefdc533647d6d7d0217
Nukepayload2 <1939357182@qq.com>
2018-03-17 01:30:13 +0800

    添加方便的方法用于模拟触摸。

--- Nukepayload2.Diagnosis.InputInjection/InjectedInputTouchInfo.vb
+++ Nukepayload2.Diagnosis.InputInjection/InjectedInputTouchInfo.vb
@@ -12,25 +12,25 @@
 ''' </summary>
 Public Structure InjectedInputTouchInfo
     ''' <summary>
-    ''' 获取或设置触控输入共有的基本指针信息。
+    ''' 触控输入共有的基本指针信息。
     ''' </summary>
     Dim PointerInfo As InjectedInputPointerInfo
     Dim TouchFlags As UInteger ' TOUCH_FLAG_NONE = 0
     ''' <summary>
-    ''' 获取或设置用于模拟触控输入的触摸状态。
+    ''' 用于模拟触控输入的触摸状态。
     ''' </summary>
     Dim TouchParameters As InjectedInputTouchParameters
     ''' <summary>
-    ''' 获取或设置表示触控接触区域的边界框的屏幕坐标。以与设备无关的像素 (DIP) 为单位。 默认值为 0 × 0 矩形，以指针 PixelLocation 为中心。
+    ''' 表示触控接触区域的边界框的屏幕坐标。以与设备无关的像素 (DIP) 为单位。 默认值为 0 × 0 矩形，以指针 PixelLocation 为中心。
     ''' </summary>
     Dim Contact As InjectedInputRectangle
     Dim ContactRaw As InjectedInputRectangle
     ''' <summary>
-    ''' 获取或设置指针设备绕长轴（z 轴，垂直于数字化仪的表面）的逆时针方向旋转角度。值介于 0 和 359 之间，其中 0 表示与 x 轴对齐并从左向右指向的触摸指针；增加值表示逆时针方向上的旋转度。 默认值为 0。
+    ''' 指针设备绕长轴（z 轴，垂直于数字化仪的表面）的逆时针方向旋转角度。值介于 0 和 359 之间，其中 0 表示与 x 轴对齐并从左向右指向的触摸指针；增加值表示逆时针方向上的旋转度。 默认值为 0。
     ''' </summary>
     Dim Orientation As UInteger
     ''' <summary>
-    ''' 获取或设置由指针设备施加在数字化仪表面上的力。规范化为 0 和 1024 之间的范围。默认值为 0。
+    ''' 由指针设备施加在数字化仪表面上的力。规范化为 0 和 1024 之间的范围。默认值为 0。
     ''' </summary>
     Dim Pressure As UInteger
 End Structure
---
+++ Nukepayload2.Diagnosis.InputInjection/MyExtension/Computer.vb
@@ -0,0 +1,138 @@
+﻿Namespace My
+
+    Public Class Computer
+        Public Shared ReadOnly Property TouchScreen As New TouchScreen
+    End Class
+
+    Public Class TouchScreen
+        Private _injection As InputInjection
+
+        Sub New()
+            _injection = InputInjection.TryCreate
+            _injection?.InitializeTouchInjection(InjectedInputVisualizationMode.Default)
+        End Sub
+
+        ''' <summary>
+        ''' 发送一次单点触摸。包括按下，按住，和松开。
+        ''' </summary>
+        ''' <param name="pos">要触摸的位置</param>
+        ''' <param name="durationMilliseconds">触摸的持续时间</param>
+        ''' <param name="orientation">指针设备绕长轴（z 轴，垂直于数字化仪的表面）的逆时针方向旋转角度。值介于 0 和 359 之间，其中 0 表示与 x 轴对齐并从左向右指向的触摸指针；增加值表示逆时针方向上的旋转度。</param>
+        ''' <param name="pointerId">指针生命周期的唯一标识符。 指针在进入检测范围时创建，并在离开检测范围时被破坏。 如果指针超出检测范围，然后返回，则将其视为新指针，并可能会分配一个新的标识符。</param>
+        ''' <param name="pressure">由指针设备施加在数字化仪表面上的力。规范化为 0 和 1024 之间的范围。默认值为 0。</param>
+        ''' <param name="areaSize">触控接触区域的边界框的大小。以与设备无关的像素 (DIP) 为单位。默认是 4x4 的矩形。</param>
+        ''' <param name="dragTo">按下屏幕期间，需要拖动到哪个点。</param>
+        ''' <param name="dragEasingX">需要拖动到的点 X 轴的缓动函数。默认是线性缓动。参数在 0 到 1 之间。</param>
+        ''' <param name="dragEasingY">需要拖动到的点 Y 轴的缓动函数。默认是线性缓动。参数在 0 到 1 之间。</param>
+        ''' <remarks>缓动函数每秒钟会被执行 10000 次甚至更多。</remarks>
+        Public Sub SendTouch(pos As (X As Integer, Y As Integer),
+                             Optional durationMilliseconds As Integer = 150,
+                             Optional orientation As Integer = 90,
+                             Optional pointerId As Integer = 0,
+                             Optional pressure As Integer = 1024,
+                             Optional areaSize As (Width As Double, Height As Double)? = Nothing,
+                             Optional dragTo As (X As Integer, Y As Integer)? = Nothing,
+                             Optional dragEasingX As Func(Of Double, Double) = Nothing,
+                             Optional dragEasingY As Func(Of Double, Double) = Nothing)
+            If durationMilliseconds < 0 Then
+                Throw New ArgumentOutOfRangeException(NameOf(durationMilliseconds), "Value can't be < 0.")
+            End If
+            If orientation < 0 OrElse orientation > 359 Then
+                Throw New ArgumentOutOfRangeException(NameOf(orientation), "Value can't be < 0 or > 359.")
+            End If
+            If pressure < 0 OrElse pressure > 1024 Then
+                Throw New ArgumentOutOfRangeException(NameOf(pressure), "Value can't be < 0 or > 1024.")
+            End If
+            Dim pxWidth As Integer
+            Dim pxHeight As Integer
+            If areaSize Is Nothing Then
+                pxWidth = 2
+                pxHeight = 2
+            Else
+                Dim sizeValue = areaSize.Value
+                If sizeValue.Width <= 0 Then
+                    Throw New ArgumentOutOfRangeException(NameOf(sizeValue), "Width can't be <= 0.")
+                End If
+                If sizeValue.Height <= 0 Then
+                    Throw New ArgumentOutOfRangeException(NameOf(sizeValue), "Height can't be <= 0.")
+                End If
+                pxWidth = sizeValue.Width / 2
+                pxHeight = sizeValue.Height / 2
+            End If
+            With _injection
+                Dim touch As New InjectedInputTouchInfo With {
+                    .PointerInfo = New InjectedInputPointerInfo With {
+                        .PointerId = pointerId,
+                        .PixelLocation = New InjectedInputPoint With {
+                            .PositionX = pos.X, ' Y co-ordinate of touch on screen
+                            .PositionY = pos.Y ' X co-ordinate of touch on screen
+                        },
+                        .PointerType = PointerInputType.Touch ' Must be set to PointerInputType.Touch. It's different from UWP.
+                    },
+                    .TouchParameters =
+                        InjectedInputTouchParameters.Contact Or
+                        InjectedInputTouchParameters.Orientation Or
+                        InjectedInputTouchParameters.Pressure, ' TouchMask
+                    .Orientation = orientation,
+                    .Pressure = pressure
+                }
+                ' defining contact area (I have taken area of 4 x 4 pixel)
+                Dim pxLoc As InjectedInputPoint = touch.PointerInfo.PixelLocation
+                touch.Contact = New InjectedInputRectangle With {
+                    .Top = pxLoc.PositionY - pxHeight,
+                    .Bottom = pxLoc.PositionY + pxHeight,
+                    .Left = pxLoc.PositionX - pxWidth,
+                    .Right = pxLoc.PositionX + pxWidth
+                }
+
+                ' TouchDown
+                touch.PointerInfo.PointerOptions =
+                    InjectedInputPointerOptions.PointerDown Or
+                    InjectedInputPointerOptions.InRange Or
+                    InjectedInputPointerOptions.InContact
+
+                .InjectTouchInput(touch) ' Injecting the touch Down from screen
+
+                ' Hold
+                touch.PointerInfo.PointerOptions =
+                    InjectedInputPointerOptions.Update Or
+                    InjectedInputPointerOptions.InRange Or
+                    InjectedInputPointerOptions.InContact
+
+                If durationMilliseconds = 0 Then
+                    Dim timer As New Stopwatch
+                    timer.Start()
+                    If dragTo Is Nothing Then
+                        Do While timer.ElapsedMilliseconds < durationMilliseconds
+                            .InjectTouchInput(touch)
+                        Loop
+                    Else
+                        Dim posX = pos.X
+                        Dim posY = pos.Y
+                        Dim dragToValue = dragTo.Value
+                        Dim toX = dragToValue.X
+                        Dim toY = dragToValue.Y
+                        If dragEasingX Is Nothing Then
+                            dragEasingX = Function(d) d
+                        End If
+                        If dragEasingY Is Nothing Then
+                            dragEasingY = Function(d) d
+                        End If
+                        Do While timer.ElapsedMilliseconds < durationMilliseconds
+                            Dim progress = timer.ElapsedMilliseconds / durationMilliseconds
+                            Dim x = posX + CInt((toX - posX) * dragEasingX(progress))
+                            Dim y = posY + CInt((toY - posY) * dragEasingY(progress))
+                            touch.PointerInfo.PixelLocation.PositionX = x
+                            touch.PointerInfo.PixelLocation.PositionY = y
+                            .InjectTouchInput(touch)
+                        Loop
+                    End If
+                    timer.Stop()
+                End If
+                ' TouchUp
+                touch.PointerInfo.PointerOptions = InjectedInputPointerOptions.PointerUp
+                .InjectTouchInput(touch) ' Injecting the touch Up from screen
+            End With
+        End Sub
+    End Class
+End Namespace
