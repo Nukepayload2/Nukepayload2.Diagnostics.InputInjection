aa8ba105ed90299194040cbe188630f792610344
Nukepayload2 <1939357182@qq.com>
2018-12-26 00:03:27 +0800

    添加 xml 注释

--- Nukepayload2.Diagnostics.InputInjection/Preview/Helpers/Interaction.vb
+++ Nukepayload2.Diagnostics.InputInjection/Preview/Helpers/Interaction.vb
@@ -1,6 +1,10 @@
 ﻿Namespace Preview
     Public Module Interaction
 
+        ''' <summary>
+        ''' 注入全局的按键按下输入
+        ''' </summary>
+        ''' <param name="keyCode">按键的扫描码。可以通过 Win32 API MapVirtualKey 得到扫描码。</param>
         Public Sub SendKeyDown(keyCode As ScanCode)
             If s_injection Is Nothing Then
                 Throw New PlatformNotSupportedException
@@ -11,6 +15,10 @@
             s_injection.InjectKeyboardInput(keyboardInfo)
         End Sub
 
+        ''' <summary>
+        ''' 注入全局的按键松开输入
+        ''' </summary>
+        ''' <param name="keyCode">按键的扫描码。可以通过 Win32 API MapVirtualKey 得到扫描码。</param>
         Public Sub SendKeyUp(keyCode As ScanCode)
             If s_injection Is Nothing Then
                 Throw New PlatformNotSupportedException
@@ -22,6 +30,11 @@
             s_injection.InjectKeyboardInput(keyboardInfo)
         End Sub
 
+        ''' <summary>
+        ''' 注入全局的按键按下然后松开输入。通常对游戏模拟输入需要用这种方式。
+        ''' </summary>
+        ''' <param name="keyCode">按键的扫描码。可以通过 Win32 API MapVirtualKey 得到扫描码。</param>
+        ''' <param name="delay">按键按下到松开的间隔 (毫秒)</param>
         Public Async Function SendKeyPressToGameAsync(keyCode As ScanCode, delay As Integer) As Task
             If s_injection Is Nothing Then
                 Throw New PlatformNotSupportedException
@@ -35,6 +48,10 @@
             s_injection.InjectKeyboardInput(keyboardInfo)
         End Function
 
+        ''' <summary>
+        ''' 注入全局的虚拟按键输入
+        ''' </summary>
+        ''' <param name="key">按键的虚拟键码。</param>
         Public Sub SendKey(key As VirtualKey)
             Dim keyboardInfo As New InjectedInputKeyboardInfo With {
                 .VirtualKey = key
@@ -42,6 +59,10 @@
             s_injection.InjectKeyboardInput(keyboardInfo)
         End Sub
 
+        ''' <summary>
+        ''' 注入全局的字符输入。用于模拟输入法和包含特殊文字的键盘。
+        ''' </summary>
+        ''' <param name="unicodeChar">按键的 Unicode 码。</param>
         Public Sub SendChar(unicodeChar As Char)
             Dim keyboardInfo As New InjectedInputKeyboardInfo With {
                 .ScanCode = AscW(unicodeChar), .KeyOptions = InjectedInputKeyOptions.Unicode
@@ -49,7 +70,12 @@
             s_injection.InjectKeyboardInput(keyboardInfo)
         End Sub
 
-        Public Async Function SendTextAsync(text As String, Optional delay As Integer = 34) As Task
+        ''' <summary>
+        ''' 注入全局的字符串输入。用于模拟输入法和包含特殊文字的键盘。
+        ''' </summary>
+        ''' <param name="text">需要输入的文本</param>
+        ''' <param name="interval">输入文本的间隔 (毫秒)</param>
+        Public Async Function SendTextAsync(text As String, Optional interval As Integer = 34) As Task
             If text = Nothing Then
                 Return
             End If
@@ -59,7 +85,7 @@
             For Each ch In text
                 keyboardInfo.ScanCode = AscW(ch)
                 s_injection.InjectKeyboardInput(keyboardInfo)
-                Await Task.Delay(delay)
+                Await Task.Delay(interval)
             Next
         End Function
     End Module
